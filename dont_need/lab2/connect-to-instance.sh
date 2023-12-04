9481eac0e2a30d34a9fa827927dfa4950577c41d
Margulya <magpid4@gmail.com>
2023-12-04 20:42:35 +0200

    lab2 update connect to instance via ssh script

--- dont_need/lab2/connect-to-instance.sh
+++ dont_need/lab2/connect-to-instance.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-ssh -i "DevOpsMargoKeys.pem" ec2-user@$1
+INSTANCE_ID=$1
+PUBLIC_DNS=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicDnsName' --output text)
+
+ssh -i "DevOpsMargoKeys.pem" ec2-user@$PUBLIC_DNS
