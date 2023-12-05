812b47a2be425695b6d46e0d0eeb30c82c883e85
Margulya <magpid4@gmail.com>
2023-12-05 20:50:12 +0200

    fix lab6

--- dont_need/lab6/script.sh
+++ dont_need/lab6/script.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-pvcreate /dev/sdc /dev/sdd /dev/sde /dev/sdf
-vgcreate vg01 /dev/sdc /dev/sdd /dev/sde /dev/sdf
+apt-get update
+apt-get install -y lvm2
 
-lvcreate -n lv1 -l 100%FREE vg01 /dev/sdc /dev/sdd
-lvcreate -n lv2 -l 100%FREE vg01 /dev/sde /dev/sdf
+pvcreate /dev/sdc /dev/sdd /dev/sde /dev/sdb
+vgcreate vg00 /dev/sdc /dev/sdd /dev/sde /dev/sdb
 
-mkfs.ext4 /dev/vg01/lv1
-mkfs.ext4 /dev/vg01/lv2
+lvcreate -n lv1 -l 100%FREE vg00 /dev/sdc /dev/sdd
+lvcreate -n lv2 -l 100%FREE vg00 /dev/sde /dev/sdb
+
+mkfs.ext4 /dev/vg00/lv1
+mkfs.ext4 /dev/vg00/lv2
 
 mkdir -p /mnt/vol1 /mnt/vol2
 
-echo '/dev/vg01/lv1 /mnt/vol1 ext4 defaults 0 0 && 
-      /dev/vg01/lv2 /mnt/vol2 ext4 defaults 0 0' >> /etc/fstab
+echo '/dev/vg00/lv1 /mnt/vol1 ext4 defaults 0 0 && 
+      /dev/vg00/lv2 /mnt/vol2 ext4 defaults 0 0' >> /etc/fstab
 
 mount -a
