5a3015150951223b5c394c18bf71ff3db7179e27
Margulya <magpid4@gmail.com>
2023-12-12 19:37:07 +0200

    UPDATE user-data for lab4

--- dont_need/lab4/user-data.sh
+++ dont_need/lab4/user-data.sh
@@ -3,9 +3,9 @@ PASSWORD=$(openssl rand -base64 12)
 PASSWORD_HASH=$(openssl passwd -6 -salt qwerty $PASSWORD)
 echo "Attention! Remember your password: $PASSWORD"
 
-useradd -p $PASSWORD_HASH -m adminuser
+useradd -p $PASSWORD_HASH -s /bin/bash -m adminuser
 usermod -aG sudo adminuser
-useradd -m poweruser
+useradd -s /bin/bash -m poweruser
 passwd -d poweruser
 echo "poweruser ALL=(ALL:ALL) /usr/sbin/iptables" >> /etc/sudoers
 usermod -aG adminuser poweruser
